3f2cb48b6d7033fd4e5e6d94610ed27b4e8db9ae
Mustafa Nevfel <mnevfel@limitra.com>
2019-07-02 03:35:03 +0300

    Datatable moved from lib folder to outputs folder.

---
+++ src/lib/outputs/datatable/datatable.component.ts
@@ -0,0 +1,312 @@
+import {Component, OnInit, Input, HostListener, ViewChild, ElementRef} from '@angular/core';
+import {SdkProviders} from '../../../../../sdk-core/src/lib/providers';
+
+
+@Component({
+  selector: 'lim-datatable',
+  templateUrl: './datatable.component.html',
+  styleUrls: ['./datatable.component.css']
+})
+export class DatatableComponent implements OnInit {
+  @Input() settings: any = {};
+
+  constructor(private providers: SdkProviders) {
+  }
+
+  ngOnInit() {
+    this.settings.Texts = {};
+
+    if (this.settings.Params.Lang) {
+      this.providers.Http.Get('assets/limitra/datatable.' + this.settings.Params.Lang + '.json').subscribe(response => {
+        this.settings.TextSource = response;
+        this.initTexts();
+        this.initIntervals();
+      });
+    } else {
+      this.settings.TextSource = {
+        SearchPlaceHolder: 'Type to search from [$DataLength] data..',
+        FilterTitle: 'Filter your data..',
+        FilterAccept: 'Filter',
+        FilterClose: 'Close',
+        Disabled: 'Disabled',
+        Seconds: 'seconds',
+        Minutes: 'minutes',
+        BulkChoice: 'Bulk Choice',
+        AutoRefresh: 'Auto Refresh',
+        FixedOperations: 'Fixed Operations'
+      };
+      this.initTexts();
+      this.initIntervals();
+    }
+    this.initTable();
+  }
+
+  private initTable() {
+    if (this.settings && this.settings.Params && this.settings.Columns) {
+      this.settings.Params.MaxLength = this.settings.Params.MaxLength || 500;
+      this.settings.Params.Sort = this.settings.Params.Sort || [];
+
+      this.resetColumns();
+      const params: any = {};
+      params.sort = this.settings.Params.Sort;
+      params.length = this.settings.Params.Length || 10;
+      params.page = this.settings.Params.Page || 1;
+      params.search = this.settings.Params.Search || '';
+      this.settings.Params.Page = params.page;
+
+      const qs = '?' + this.providers.Url.Serialize(params);
+      this.settings.HasProcess = true;
+      this.providers.Http.Get(this.settings.Params.Source + qs).subscribe(response => {
+        this.settings.Response = response;
+        this.settings.Response.Data.Source = this.settings.Response.Data.Source.map(data => {
+          data.PrimaryKey = this.valOfObj(data, {Field: this.settings.PrimaryKey}, false);
+          data.Columns = [];
+          this.settings.Columns.forEach(column => {
+            const colObj: any = {Nested: []};
+            this.pushColumnLen(column, column.Title.toString().length);
+            colObj.Value = this.valOfObj(data, column);
+
+            if (column.Nested) {
+              column.Nested.forEach(nest => {
+                const nestObj = {Title: nest.Title, Value: this.valOfObj(data, nest, false)};
+                colObj.Nested.push(nestObj);
+                this.pushColumnLen(column, (nest.Title.toString().length + nestObj.Value.length));
+              });
+            }
+
+            data.Columns.push(colObj);
+          });
+
+          return data;
+        });
+        this.resizeColumns();
+        this.initTexts();
+        this.setBulkChoice(false);
+        this.settings.HasProcess = false;
+        this.reCalcPage();
+      }, () => {
+        this.settings.HasProcess = false;
+      });
+    }
+  }
+
+  private initTexts() {
+    for (const text in this.settings.TextSource) {
+      this.settings.Texts[text] = this.settings.TextSource[text]
+        .replace('[$DataLength]', ((this.settings && this.settings.Response) ? this.settings.Response.Data.Length.toLocaleString() : 0))
+        .replace('[$PageLength]', ((this.settings && this.settings.Response) ? this.settings.Response.Page.Length.toLocaleString() : 0));
+    }
+  }
+
+  private initIntervals() {
+    this.settings.Intervals = [
+      {Interval: 0, Text: this.settings.TextSource.Disabled, Checked: true},
+      {Interval: 10, Text: '10 ' + this.settings.TextSource.Seconds},
+      {Interval: 30, Text: '30 ' + this.settings.TextSource.Seconds},
+      {Interval: 60, Text: '60 ' + this.settings.TextSource.Seconds},
+      {Interval: 300, Text: '5 ' + this.settings.TextSource.Minutes},
+    ];
+  }
+
+  private validateSearch() {
+    if (!this.settings.HasProcess) {
+      if (this.settings && this.settings.Params) {
+        this.settings.Params.Page = 1;
+      }
+      this.initTable();
+    }
+  }
+
+  private validatePage() {
+    if (!this.settings.HasProcess) {
+      const len = parseInt(this.settings.Params.Page, 0);
+      if (!this.settings.Params.Page || this.settings.Params.Page < 0 || !len) {
+        this.settings.Params.Page = 1;
+      }
+
+      this.initTable();
+    }
+  }
+
+  private validateLength() {
+    if (this.settings && !this.settings.HasProcess) {
+      const len = parseInt(this.settings.Params.Length, 0);
+      if (!this.settings.Params.Length || this.settings.Params.Length < 0 || !len) {
+        this.settings.Params.Length = 1;
+      }
+
+      if (this.settings.Params.MaxLength <= this.settings.Params.Length) {
+        this.settings.Params.Length = this.settings.Params.MaxLength;
+      }
+
+      this.initTable();
+    }
+  }
+
+  private reCalcPage() {
+    if (this.settings && this.settings.Response && this.settings.Params) {
+      if (this.settings.Response.Page.Length === 0 && this.settings.Params.Page > 1) {
+        this.settings.Params.Page--;
+        this.validatePage();
+      }
+    }
+  }
+
+  private goToPage(page: number) {
+    if (!this.settings.HasProcess) {
+      this.settings.Params.Page = page;
+      this.initTable();
+    }
+  }
+
+  private nextPage() {
+    if (!this.settings.HasProcess && this.settings.Response.Page.Number < this.settings.Response.Page.Count) {
+      this.goToPage(this.settings.Response.Page.Number + 1);
+    }
+  }
+
+  private privPage() {
+    if (!this.settings.HasProcess && this.settings.Response.Page.Number > 1) {
+      this.goToPage(this.settings.Response.Page.Number - 1);
+    }
+  }
+
+  private pushColumnLen(column: any, len: number) {
+    if (!column.MaxChar || column.MaxChar < len) {
+      column.MaxChar = len;
+    }
+  }
+
+  private resetColumns() {
+    if (this.settings && this.settings.Columns) {
+      this.settings.Columns = this.settings.Columns.map(column => {
+        const nwCol = column;
+        nwCol.MaxChar = undefined;
+        nwCol.Width = undefined;
+        return nwCol;
+      });
+    }
+  }
+
+  private sortColumn(column: any) {
+    if (this.settings && this.settings.Params && column) {
+      column.Direction = column.Direction || '';
+      column.Direction = column.Direction === 'asc' ? 'desc' : (column.Direction === 'desc' ? '' : 'asc');
+
+      const current = this.settings.Params.Sort.filter(x => x.includes(column.Field))[0];
+      if (current) {
+        const index = this.settings.Params.Sort.indexOf(current);
+        this.settings.Params.Sort.splice(index, 1);
+      }
+
+      if (column.Direction === 'asc' || column.Direction === 'desc') {
+        this.settings.Params.Sort.push(column.Field + ',' + column.Direction);
+      }
+      this.initTable();
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  private resizeColumns() {
+    if (this.settings && this.settings.Columns) {
+      const totalChar = this.settings.Columns.reduce((sum, current) => sum + current.MaxChar, 0);
+      this.settings.Columns.forEach(col => {
+        const width = col.MaxChar * 100 / totalChar;
+        if (width < 3) {
+          col.Width = 3;
+        } else if (this.settings.Columns.length >= 4 && width > 50) {
+          col.Width = 50;
+        } else if (this.settings.Columns.length === 3 && width > 65) {
+          col.Width = 65;
+        } else if (this.settings.Columns.length === 2 && width > 80) {
+          col.Width = 80;
+        } else {
+          col.Width = width;
+        }
+      });
+    }
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  private keyEvent(event: KeyboardEvent) {
+    if (!this.settings.HasProcess) {
+      switch (event.keyCode) {
+        case 37:
+          this.privPage();
+          break;
+        case 39:
+          this.nextPage();
+          break;
+        case 107:
+          this.settings.Params.Length++;
+          this.validateLength();
+          break;
+        case 109:
+          this.settings.Params.Length--;
+          this.validateLength();
+          break;
+        case 27:
+          this.setBulkChoice();
+          break;
+      }
+    }
+  }
+
+  private valOfObj(obj: any, column: any, effect: boolean = true): any {
+    const field = column.Field || '';
+    if (field.includes('.')) {
+      const partials = field.split('.');
+      partials.forEach(partial => {
+        if (obj) {
+          obj = obj[partial];
+        }
+      });
+    } else {
+      obj = obj[field];
+    }
+    if (effect) {
+      this.pushColumnLen(column, (obj ? obj.toString().length : 0));
+    }
+    return obj;
+  }
+
+  private setInterval(interval: any, $event) {
+    $event.stopPropagation();
+    if (this.settings && this.settings.Interval) {
+      clearInterval(this.settings.Interval);
+    }
+
+    if (interval && this.settings) {
+      this.settings.Intervals = this.settings.Intervals.map(x => {
+        x.Checked = false;
+        return x;
+      });
+      interval.Checked = true;
+      if (interval.Interval > 0) {
+        this.settings.Interval = setInterval(() => {
+          this.initTable();
+        }, (interval.Interval * 1000));
+      }
+    }
+  }
+
+  private setBulkChoice(enabled: boolean = null) {
+    this.settings.BulkChoice = enabled === false ? enabled : (enabled || !this.settings.BulkChoice);
+    if (this.settings && this.settings.Response && this.settings.Response.Data && this.settings.Response.Data.Source) {
+      this.settings.Response.Data.Source = this.settings.Response.Data.Source.map(x => {
+        x.Selected = false;
+        return x;
+      });
+    }
+  }
+
+  private selectRow(row: any) {
+    if (this.settings.BulkChoice) {
+      if (row.Selected !== false && !row.Selected) {
+        row.Selected = false;
+      }
+
+      row.Selected = !row.Selected;
+    }
+  }
+}
