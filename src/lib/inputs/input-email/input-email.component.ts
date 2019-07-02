d0929e875853a6e1e0a9e278529bc21b38877872
Mustafa Nevfel <mnevfel@limitra.com>
2019-07-02 03:52:43 +0300

    Email type input was extended from InputExtend. -Customized.

---
+++ src/lib/inputs/input-email/input-email.component.ts
@@ -0,0 +1,53 @@
+import {AfterViewInit, Component, Input} from '@angular/core';
+import {SdkProviders} from '../../../../../sdk-core/src/lib/providers';
+import {InputExtend} from '../../extends/InputExtend';
+
+@Component({
+  selector: 'lim-input-email',
+  templateUrl: './input-email.component.html',
+  styleUrls: ['./input-email.component.css']
+})
+export class InputEmailComponent extends InputExtend implements AfterViewInit {
+  constructor(protected providers: SdkProviders) { super(providers); }
+
+  @Input() minlength = 0;
+  @Input() maxlength = 100;
+
+  ngAfterViewInit() {
+    this.init();
+  }
+
+  validation(value: any) {
+    const regex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/i;
+
+    if (!regex.test(value)) {
+      this.addFormError('ValidError');
+    } else {
+      this.removeFormError('ValidError');
+    }
+
+    if (value.length < this.minlength) {
+      this.addFormError('MinLength');
+    } else {
+      this.removeFormError('MinLength');
+    }
+
+    if (value.length > this.maxlength) {
+      this.addFormError('MaxLength');
+
+      if (value.length - this.maxlength > 1) {
+        this.input.nativeElement.value = this.input.nativeElement.value.substring(0, this.maxlength + 1);
+        this.value = this.input.nativeElement.value;
+        this.valueChange.emit(this.value);
+      }
+    } else {
+      this.removeFormError('MaxLength');
+    }
+  }
+
+  localizeReplace(message: string): string {
+    message = this.providers.String.Replace(message, '[$MinLength]', this.minlength ? this.minlength.toString() : '');
+    message = this.providers.String.Replace(message, '[$MaxLength]', this.maxlength ? this.maxlength.toString() : '');
+    return message;
+  }
+}
