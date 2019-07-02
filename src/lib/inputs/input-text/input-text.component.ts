3c38bde77629ae7fe3f05f100d1a518ca2c7124d
Mustafa Nevfel <mnevfel@limitra.com>
2019-07-02 03:53:56 +0300

    Text type input was extended from InputExtend. -Customized.

---
+++ src/lib/inputs/input-text/input-text.component.ts
@@ -0,0 +1,45 @@
+import {AfterViewInit, Component, Input} from '@angular/core';
+import {InputExtend} from '../../extends/InputExtend';
+import {SdkProviders} from '../../../../../sdk-core/src/lib/providers';
+
+@Component({
+  selector: 'lim-input-text',
+  templateUrl: './input-text.component.html',
+  styleUrls: ['./input-text.component.css']
+})
+export class InputTextComponent extends InputExtend implements AfterViewInit {
+  constructor(protected providers: SdkProviders) { super(providers); }
+
+  @Input() minlength = 0;
+  @Input() maxlength = 50;
+
+  ngAfterViewInit() {
+    this.init();
+  }
+
+  validation(value: any) {
+    if (value.length < this.minlength) {
+      this.addFormError('MinLength');
+    } else {
+      this.removeFormError('MinLength');
+    }
+
+    if (value.length > this.maxlength) {
+      this.addFormError('MaxLength');
+
+      if (value.length - this.maxlength > 1) {
+        this.input.nativeElement.value = this.input.nativeElement.value.substring(0, this.maxlength + 1);
+        this.value = this.input.nativeElement.value;
+        this.valueChange.emit(this.value);
+      }
+    } else {
+      this.removeFormError('MaxLength');
+    }
+  }
+
+  localizeReplace(message: string): string {
+    message = this.providers.String.Replace(message, '[$MinLength]', this.minlength ? this.minlength.toString() : '');
+    message = this.providers.String.Replace(message, '[$MaxLength]', this.maxlength ? this.maxlength.toString() : '');
+    return message;
+  }
+}
