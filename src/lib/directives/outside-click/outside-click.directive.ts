be4e36b04a2a9c7b2bd9fa25c57adb5c300d21de
Mustafa Nevfel <mnevfel@limitra.com>
2019-07-03 22:30:52 +0300

    Outside click event directive pinned. lim-os-click

---
+++ src/lib/directives/outside-click/outside-click.directive.ts
@@ -0,0 +1,17 @@
+import {Directive, ElementRef, EventEmitter, HostListener, Output} from '@angular/core';
+
+@Directive({
+  selector: '[lim-os-click]'
+})
+export class OutsideClickDirective {
+  constructor(private elementRef: ElementRef) { }
+
+  @Output('lim-os-click') clickOutside: EventEmitter<any> = new EventEmitter();
+
+  @HostListener('document:click', ['$event.target']) onMouseEnter(targetElement) {
+    const clickedInside = this.elementRef.nativeElement.contains(targetElement);
+    if (!clickedInside) {
+      this.clickOutside.emit(null);
+    }
+  }
+}
