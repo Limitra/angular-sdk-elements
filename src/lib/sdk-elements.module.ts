ab6470c1bf6d8598c4fea66fb8f041f67722fa38
Mustafa Nevfel <mnevfel@limitra.com>
2019-07-03 17:59:19 +0300

    Select for client imported to elements module.

--- src/lib/sdk-elements.module.ts
+++ src/lib/sdk-elements.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
-import {InputDateComponent} from './input-date/input-date.component';
-import { DatatableComponent } from './datatable/datatable.component';
+import {InputDateComponent} from './inputs/input-date/input-date.component';
+import { DatatableComponent } from './outputs/datatable/datatable.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import {SdkProviders} from '../../../sdk-core/src/lib/providers';
 import {SdkCoreModule} from '../../../sdk-core/src/lib/sdk-core.module';
 import {FormsModule} from '@angular/forms';
+import { InputTextComponent } from './inputs/input-text/input-text.component';
+import { InputPasswordComponent } from './inputs/input-password/input-password.component';
+import { InputPhoneComponent } from './inputs/input-phone/input-phone.component';
+import { InputEmailComponent } from './inputs/input-email/input-email.component';
+import { SelectClientComponent } from './inputs/select-client/select-client.component';
 
 @NgModule({
   imports: [
@@ -15,7 +20,7 @@ import {FormsModule} from '@angular/forms';
     FormsModule
   ],
   providers: [SdkProviders],
-  declarations: [InputDateComponent, DatatableComponent],
-  exports: [InputDateComponent, DatatableComponent]
+  declarations: [InputDateComponent, DatatableComponent, InputTextComponent, InputPasswordComponent, InputPhoneComponent, InputEmailComponent, SelectClientComponent],
+  exports: [InputDateComponent, DatatableComponent, InputTextComponent, InputPasswordComponent, InputPhoneComponent, InputEmailComponent, SelectClientComponent]
 })
 export class SdkElementsModule { }
