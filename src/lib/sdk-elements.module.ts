8646006781942877f2cc41c9e8df2fe57f1208e6
Mustafa Nevfel <mnevfel@limitra.com>
2019-07-06 03:00:50 +0300

    Select server input component added to sdk elements module.

--- src/lib/sdk-elements.module.ts
+++ src/lib/sdk-elements.module.ts
@@ -12,6 +12,7 @@ import { InputPhoneComponent } from './inputs/input-phone/input-phone.component'
 import { InputEmailComponent } from './inputs/input-email/input-email.component';
 import { SelectClientComponent } from './inputs/select-client/select-client.component';
 import {OutsideClickDirective} from './directives/outside-click/outside-click.directive';
+import { SelectServerComponent } from './inputs/select-server/select-server.component';
 
 @NgModule({
   imports: [
@@ -21,7 +22,7 @@ import {OutsideClickDirective} from './directives/outside-click/outside-click.di
     FormsModule
   ],
   providers: [SdkProviders],
-  declarations: [OutsideClickDirective, InputDateComponent, DatatableComponent, InputTextComponent, InputPasswordComponent, InputPhoneComponent, InputEmailComponent, SelectClientComponent],
-  exports: [OutsideClickDirective, InputDateComponent, DatatableComponent, InputTextComponent, InputPasswordComponent, InputPhoneComponent, InputEmailComponent, SelectClientComponent]
+  declarations: [OutsideClickDirective, InputDateComponent, DatatableComponent, InputTextComponent, InputPasswordComponent, InputPhoneComponent, InputEmailComponent, SelectClientComponent, SelectServerComponent],
+  exports: [OutsideClickDirective, InputDateComponent, DatatableComponent, InputTextComponent, InputPasswordComponent, InputPhoneComponent, InputEmailComponent, SelectClientComponent, SelectServerComponent]
 })
 export class SdkElementsModule { }
