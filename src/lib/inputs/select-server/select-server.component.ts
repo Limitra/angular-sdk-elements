3adaef240a1c48bbce2f0b0524e0048352a0484e
Mustafa Nevfel <mnevfel@limitra.com>
2019-07-06 03:00:07 +0300

    Select server input logic pinned for now. -BugsForProd.

---
+++ src/lib/inputs/select-server/select-server.component.ts
@@ -0,0 +1,138 @@
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {InputExtend} from '../../extends/InputExtend';
+import {SdkProviders} from '../../../../../sdk-core/src/lib/providers';
+
+@Component({
+  selector: 'lim-select-server',
+  templateUrl: './select-server.component.html',
+  styleUrls: ['./select-server.component.css']
+})
+export class SelectServerComponent extends InputExtend implements AfterViewInit {
+  constructor(protected providers: SdkProviders) { super(providers); }
+
+  @Input() source: string;
+  @Input() multiple = false;
+  @Input() minlength: number;
+  @Input() maxlength: number;
+
+  @Input() length: number;
+
+  @Input() textkey: string;
+  @Input() valuekey: string;
+
+  @ViewChild('search', {static: false}) search: ElementRef;
+
+  private page = 1;
+  private selected: any;
+  private selecteds: Array<any> = [];
+  private searchText: string;
+  private filteredSource: Array<any> = [];
+
+  ngAfterViewInit() {
+    this.init();
+    this.initSource(true, false);
+  }
+
+  initSource(init: boolean, reset: boolean) {
+    const params: any = { page: this.page };
+    if (this.length) { params.length = this.length; }
+    if (this.searchText) { params.search = this.searchText.toLowerCase(); }
+    if (this.value) { params.value = this.value; }
+    const source = this.source + '?' + this.providers.Url.Serialize(params);
+    this.providers.Http.Get(source).subscribe(response => {
+      if (reset) {
+        this.filteredSource = response.Data.Source;
+      } else {
+        this.filteredSource = this.filteredSource.concat(response.Data.Source);
+      }
+      if (init) {
+        if (this.multiple) {
+          this.selecteds = this.filteredSource.filter(x => this.value
+            ? (this.value.filter(y => y == x[this.valuekey]).length > 0)
+            : false).map(x => x[this.valuekey]);
+        } else {
+          this.selected = this.filteredSource.filter(x => this.value ? this.value == x[this.valuekey] : false)[0];
+        }
+      }
+    });
+  }
+
+  scroll(event) {
+    if (event.target.scrollTop === event.target.scrollTopMax) {
+      this.page++;
+      this.initSource(false, false);
+    }
+  }
+
+  forceValue() {
+    if (this.input.nativeElement.value) {
+      if (this.multiple) {
+        const values: Array<any> = this.input.nativeElement.value.split(',') || [];
+        this.value = values;
+      } else  {
+        this.value = this.input.nativeElement.value;
+      }
+      this.valueChange.emit(this.value);
+    } else {
+      this.value = (this.multiple ? [] : undefined);
+      this.valueChange.emit(this.value);
+    }
+  }
+
+  validation(value: any) {
+    if (this.multiple) {
+      const values: Array<any> = this.input.nativeElement.value.split(',') || [];
+      if (this.minlength && values.length < this.minlength) {
+        this.addFormError('SelectMinLength');
+      } else {
+        this.removeFormError('SelectMinLength');
+      }
+
+      if (this.maxlength && values.length > this.maxlength) {
+        this.addFormError('SelectMaxLength');
+      } else {
+        this.removeFormError('SelectMaxLength');
+      }
+    }
+  }
+
+  removeValue() {
+    this.input.nativeElement.value = '';
+    this.selected = undefined;
+    this.selecteds = [];
+    this.validate();
+  }
+
+  searchValue() {
+    if (this.source) {
+      this.page = 1;
+      this.initSource(false, true);
+    }
+  }
+
+  checkOption(option: any) {
+    this.search.nativeElement.focus();
+    if (option && option[this.valuekey]) {
+      if (this.multiple) {
+        const selected = this.selecteds.filter(x => x === option[this.valuekey])[0];
+        if (!selected) {
+          this.selecteds.push(option[this.valuekey]);
+        } else {
+          this.selecteds.splice(this.selecteds.indexOf(selected), 1);
+        }
+        this.input.nativeElement.value = this.selecteds;
+      } else {
+        this.selected = option;
+        this.input.nativeElement.value = option[this.valuekey];
+      }
+
+      this.validate();
+    }
+  }
+
+  localizeReplace(message: string): string {
+    message = this.providers.String.Replace(message, '[$MinLength]', this.minlength ? this.minlength.toString() : '');
+    message = this.providers.String.Replace(message, '[$MaxLength]', this.maxlength ? this.maxlength.toString() : '');
+    return message;
+  }
+}
