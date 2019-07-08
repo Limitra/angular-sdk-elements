682f150abeff45a2637624f05adeda32cc6329e5
Mustafa Nevfel <mnevfel@limitra.com>
2019-07-08 18:23:20 +0300

    Input choice spec file added.

---
+++ src/lib/inputs/input-choice/input-choice.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InputChoiceComponent } from './input-choice.component';
+
+describe('InputChoiceComponent', () => {
+  let component: InputChoiceComponent;
+  let fixture: ComponentFixture<InputChoiceComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ InputChoiceComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InputChoiceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
