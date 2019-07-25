1d2ff56369503a0a4d15a441fedddf7ea5f69800
Mustafa Nevfel <mnevfel@limitra.com>
2019-07-25 16:00:00 +0000

    Lim card component logic generated.

---
+++ src/lib/outputs/card/card.component.ts
@@ -0,0 +1,40 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {SdkProviders} from '@limitra/sdk-core';
+
+@Component({
+  selector: 'lim-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  @Input() icon: string;
+  @Input() title: string;
+  @Input() button: any = { Primary: [], Secondary: [] };
+
+  texts: any;
+  collapsed: boolean;
+
+  private lang: string;
+
+  constructor(private providers: SdkProviders) {
+  }
+
+  ngOnInit() {
+    this.lang = this.lang || this.providers.Storage.Get('Localization_Lang');
+    this.button = this.button || {};
+
+    this.texts = {};
+
+    if (this.lang) {
+      this.providers.Http.Get('assets/limitra/interface.' + this.lang + '.json').subscribe(response => {
+        this.texts = response;
+      });
+    } else {
+      this.texts = {
+        CardOptions: 'Card Options',
+        CardCollapse: 'Collapse Card',
+        CardExpand: 'Expand Card'
+      };
+    }
+  }
+}
