9378821c9a8d05a2ce502b0e20b277982c3d44c0
mnevfel <mnevfel@limitra.com>
2019-08-04 21:26:06 +0300

    Request to json result function added to source.

--- src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of sdk-elements
- */
-
-export * from '@limitra/sdk-core/public-api';
-
-export * from './lib/sdk-elements.module';
-export * from './lib/extends/input-extend';
