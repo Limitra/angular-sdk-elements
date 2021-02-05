2f43212191c93fc469d4cf5c3adc89bf3f78d93d
Mustafa Nevfel <mnevfel@limitra.com>
2021-02-06 01:36:51 +0300

    Topbar image domain bug fixed.

--- src/lib/outputs/top-bar/top-bar.component.ts
+++ src/lib/outputs/top-bar/top-bar.component.ts
@@ -43,7 +43,7 @@ export class TopBarComponent implements OnInit, OnDestroy {
         this.providers.Http.Get(api.Domain + '/' + this.profile).subscribe(response => {
           this.profileObj.DisplayName = response.DisplayName;
           if (api.File && api.File.Download && response.Picture) {
-            this.profileObj.Picture = api.Domain + '/' + api.File.Download + response.Picture;
+            this.profileObj.Picture = (api.File.Domain || api.Domain) + '/' + api.File.Download + response.Picture;
           }
         });
       };
